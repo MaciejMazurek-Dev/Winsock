6ce4c295c5c19198d6ddf6ab932955ccb726fde2
Maciej Mazurek <157367207+MaciejMazurek-Dev@users.noreply.github.com>
2025-12-07 14:06:39 +0100

    Add project files.

---
+++ WinsockServer/main.c
@@ -0,0 +1,15 @@
+#ifndef WIN32_LEAN_AND_MEAN
+#define WIN32_LEAN_AND_MEAN
+#endif
+
+#include <WinSock2.h>
+#include <WS2tcpip.h>
+#include <stdio.h>
+
+#pragma comment(lib, "Ws2_32.lib")
+
+
+int main()
+{
+	return 0;
+}
