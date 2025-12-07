dccee5db989fa1d7493d53a0903f870d2dc30f1f
Maciej Mazurek <157367207+MaciejMazurek-Dev@users.noreply.github.com>
2025-12-07 17:44:36 +0100

    Add basic Winsock initialization

--- WinsockServer/main.c
+++ WinsockServer/main.c
@@ -1,15 +1,41 @@
-#ifndef WIN32_LEAN_AND_MEAN
+//WIN32_LEAN_AND_MEAN macro prevents the Winsock.h from being included by the Windows.h header.
 #define WIN32_LEAN_AND_MEAN
-#endif
 
 #include <WinSock2.h>
+#include <Windows.h>
 #include <WS2tcpip.h>
+#include <iphlpapi.h>
 #include <stdio.h>
 
+//Winsock functions are declared in header file <winsock2.h>, but the actual code that implements them lives in Ws2_32.lib library file.
+//Line - #pragma comment(lib, "Ws2_32.lib") - automatically link the Winsock library so socket functions work.
 #pragma comment(lib, "Ws2_32.lib")
+
 
 
 int main()
 {
+	WORD windowsSocketVersion = MAKEWORD(2, 2); //We want Winsock in version 2.2
+	WSADATA wsaData; //WSADATA structure contains information about the Windows Sockets implementation
+	int result; 
+
+	printf("Initializing Winsock...");
+	result = WSAStartup(windowsSocketVersion, &wsaData); //Initiate use of WS2_32.dll. Returns 0 if OS supports Winsock version 2.2
+	if (result != 0)
+	{
+		result = WSAStartup(windowsSocketVersion, &wsaData);
+		printf("WSAStartup failed  with error: %d\n", result);
+		return 1;
+	}
+	
+	//Cleanup
+	result = WSACleanup();
+	if (result != 0)
+	{
+		printf("Cleanup failed with error: %d\n", result);
+		return 1;
+	}
+
+
 	return 0;
 }
