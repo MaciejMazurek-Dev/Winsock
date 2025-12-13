b72728110c2fba59e7bfc8f007e744670908a69a
Maciej Mazurek <157367207+MaciejMazurek-Dev@users.noreply.github.com>
2025-12-13 18:07:01 +0100

    Add address binding

--- WinsockServer/main.c
+++ WinsockServer/main.c
@@ -1,34 +1,68 @@
-//WIN32_LEAN_AND_MEAN macro prevents the Winsock.h from being included by the Windows.h header.
+//WIN32_LEAN_AND_MEAN macro prevents the old version 1 of Winsock (Winsock.h) from being included by the Windows.h header which conflicts with new version in header file winsock2.h
 #define WIN32_LEAN_AND_MEAN
 
-#include <WinSock2.h>
 #include <Windows.h>
+#include <WinSock2.h>
 #include <WS2tcpip.h>
 #include <iphlpapi.h>
 #include <stdio.h>
 
 //Winsock functions are declared in header file <winsock2.h>, but the actual code that implements them lives in Ws2_32.lib library file.
-//Line - #pragma comment(lib, "Ws2_32.lib") - automatically link the Winsock library so socket functions work.
+//Pragma line below automatically link the Winsock library so socket functions work.
 #pragma comment(lib, "Ws2_32.lib")
+
+
+#define IPV4_ADDRESS "127.0.0.1"
+#define PORT 6666
+
 
 
 
 int main()
 {
-	WORD windowsSocketVersion = MAKEWORD(2, 2); //We want Winsock in version 2.2
-	WSADATA wsaData; //WSADATA structure contains information about the Windows Sockets implementation
-	int result; 
-
+	//INITIALIZE
+	WORD windowsSocketVersion = MAKEWORD(2, 2);
+	//WSADATA contains information about the Windows Sockets implementation. Btw. WSA means WinSock API
+	WSADATA wsaData;
 	printf("Initializing Winsock...");
-	result = WSAStartup(windowsSocketVersion, &wsaData); //Initiate use of WS2_32.dll. Returns 0 if OS supports Winsock version 2.2
+	//WSAStartup initiate use of WS2_32.dll (Winsock DLL) and returns 0 if everythings is OK.
+	int result = WSAStartup(windowsSocketVersion, &wsaData); 
 	if (result != 0)
 	{
-		result = WSAStartup(windowsSocketVersion, &wsaData);
-		printf("WSAStartup failed  with error: %d\n", result);
+		printf("WSAStartup failed with error: %d\n", result);
 		return 1;
 	}
 	
-	//Cleanup
+
+	//SETUP SERVER
+	SOCKET listenSocket = INVALID_SOCKET;
+	listenSocket = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+	if (listenSocket == INVALID_SOCKET)
+	{
+		printf("Socket construction failed with error: %d\n", WSAGetLastError());
+		WSACleanup();
+		return 1;
+	}
+
+	//Bind to the address
+	struct sockaddr_in serverAddress;
+	serverAddress.sin_family = AF_INET;
+	serverAddress.sin_addr.S_un.S_addr = inet_addr(IPV4_ADDRESS);
+	serverAddress.sin_port = htons(PORT);
+	result = bind(listenSocket, &serverAddress, sizeof(serverAddress));
+	if (result == SOCKET_ERROR)
+	{
+		printf("Binding a socket failed with error %d\n", WSAGetLastError());
+		closesocket(listenSocket);
+		WSACleanup();
+		return 1;
+	}
+
+
+
+
+	//CLEANUP
+	printf("Cleaning up...");
 	result = WSACleanup();
 	if (result != 0)
 	{
