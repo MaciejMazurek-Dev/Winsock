56c349789018450723e335fd5a3d6f125333e468
Maciej Mazurek <157367207+MaciejMazurek-Dev@users.noreply.github.com>
2025-12-25 20:47:41 +0100

    Add error check when using inet_pton()

--- WinsockServer/main.c
+++ WinsockServer/main.c
@@ -1,4 +1,4 @@
-//WIN32_LEAN_AND_MEAN macro prevents the old version 1 of Winsock (Winsock.h) from being included by the Windows.h header which conflicts with new version in header file winsock2.h
+// WIN32_LEAN_AND_MEAN macro prevents the old version 1 of Winsock (Winsock.h) from being included by the Windows.h header which conflicts with new version in header file winsock2.h
 #define WIN32_LEAN_AND_MEAN
 
 #include <Windows.h>
@@ -7,8 +7,8 @@
 #include <iphlpapi.h>
 #include <stdio.h>
 
-//Winsock functions are declared in header file <winsock2.h>, but the actual code that implements them lives in Ws2_32.lib library file.
-//Pragma line below automatically link the Winsock library so socket functions work.
+// Winsock functions are declared in header file <winsock2.h>, but the actual code that implements them lives in Ws2_32.lib library file.
+// Pragma line below automatically link the Winsock library so socket functions work.
 #pragma comment(lib, "Ws2_32.lib")
 
 #define IPV4_ADDRESS "127.0.0.1"
@@ -17,21 +17,20 @@
 
 int main()
 {
-	//INITIALIZE
+	// Initiate Winsock
 	WORD windowsSocketVersion = MAKEWORD(2, 2);
-	//WSADATA contains information about the Windows Sockets implementation. Btw. WSA means WinSock API
+	// WSADATA contains information about the Windows Sockets implementation. Btw. WSA means WinSock API
 	WSADATA wsaData;
 	printf("Initializing Winsock\n");
-	//WSAStartup initiate use of WS2_32.dll (Winsock DLL) and returns 0 if everythings is OK.
+	//Initiate use of WS2_32.dll which contains WinSock 2 implementation and returns 0 if everythings is OK.
 	int result = WSAStartup(windowsSocketVersion, &wsaData); 
 	if (result != 0)
 	{
 		printf("WSAStartup failed with error: %d\n", result);
 		return 1;
 	}
-	
 
-	//SERVER SETUP
+	// Setup Socket - IPv4 address family, Stream Socket, TCP protocol
 	SOCKET listenSocket = INVALID_SOCKET;
 	listenSocket = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
 	if (listenSocket == INVALID_SOCKET)
@@ -41,11 +40,27 @@ int main()
 		return 1;
 	}
 
-	//Bind Socket to the address
+	// Bind the socket to an IPv4 address
 	struct sockaddr_in serverAddress;
 	serverAddress.sin_family = AF_INET;
-	//Convert IP text address into binary network format
-	inet_pton(AF_INET, (PCWSTR)(IPV4_ADDRESS), &serverAddress.sin_addr.S_un.S_addr);
+	// Convert the string IP address to binary network format. pton stands for "presentation to network"
+	result = inet_pton(AF_INET, (PCWSTR)(IPV4_ADDRESS), &serverAddress.sin_addr); // &serverAddress.sin_addr.S_un.S_addr
+	if (result == 0)
+	{
+		printf("Provided IP address is not valid.");
+		closesocket(listenSocket);
+		WSACleanup();
+		return 1;
+	}
+	else if(result < 0)
+	{
+		printf("Converting IP address to network format failed with error: %d", WSAGetLastError());
+		closesocket(listenSocket);
+		WSACleanup();
+		return 1;
+	}
+
+	// Convert from host byte order to network byte order (big-endian).
 	serverAddress.sin_port = htons(PORT);
 	result = bind(listenSocket, &serverAddress, sizeof(serverAddress));
 	if (result == SOCKET_ERROR)
@@ -56,7 +71,7 @@ int main()
 		return 1;
 	}
 
-	//Listening on a Socket
+	// Listen for incoming connections
 	result = listen(listenSocket, SOMAXCONN);
 	if (result == SOCKET_ERROR)
 	{
@@ -65,9 +80,9 @@ int main()
 		WSACleanup();
 		return 1;
 	}
-
-	//Accepting a connection
 	printf("Listening for incoming connections...\n");
+
+	// Accept an incoming connection
 	SOCKET clientSocket;
 	clientSocket = INVALID_SOCKET;
 	clientSocket = accept(listenSocket, NULL, NULL);
@@ -78,8 +93,8 @@ int main()
 		WSACleanup();
 		return 1;
 	}
-
-	//Receiving data
+	
+	// Receive data from the client connection
 	char receivingBuffer[BUFFER_LENGTH];
 	int bufferLength = BUFFER_LENGTH;
 	int receiveResult;
@@ -103,7 +118,7 @@ int main()
 
 
 
-	//CLEANUP
+	// Close Sockets and free allocated system resources 
 	printf("Cleaning up...\n");
 	closesocket(listenSocket);
 	closesocket(clientSocket);
